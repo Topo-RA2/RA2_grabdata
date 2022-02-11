469bd36a363734b4a6fcc1bd94c3cf83ce06d037
TTopoo <826955320@qq.com>
2022-02-11 23:19:33 +0800

    加一个设置页以及游戏内时间判断
    优化一下数据显示

---
+++ setting.cpp
@@ -0,0 +1,39 @@
+﻿#include "setting.h"
+#include "ui_setting.h"
+extern int cashWarning;
+Setting::Setting(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Setting)
+{
+    ui->setupUi(this);
+    ui->textEdit->setText(QString::number(cashWarning));
+}
+
+Setting::~Setting()
+{
+    delete ui;
+}
+
+void Setting::on_pushButton_clicked()
+{
+    QString text = ui->textEdit->toPlainText();
+    bool ok;
+    int tmp = text.toInt(&ok);
+    if(ok)
+    {
+        cashWarning = tmp;
+        QMessageBox::information(NULL, NULL, "success", QMessageBox::Yes);
+        this->close();
+    }
+    else
+    {
+        QMessageBox::information(NULL, NULL, "fail", QMessageBox::Yes);
+    }
+}
+
+
+void Setting::on_pushButton_2_clicked()
+{
+    this->close();
+}
+
